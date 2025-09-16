80cf98dba16a1eee3e019574fc69b43e57069526
quijote85 <quijote85@gmail.com>
2025-09-16 13:45:44 -0600

    feat: Add variables at Python File

--- programa_Python.py
+++ programa_Python.py
@@ -1,5 +1,9 @@
 ##Mostrar cambio en el repositorio
+
+var_Nombre="Marco"
+var_apPaterno="Pardo"
+var_apMaternio="Meraza"
+
 print("Prueba de programa en Python")
 
 print("Segunda linea de codigo")
-
